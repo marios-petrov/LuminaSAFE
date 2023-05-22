2b40da3acb8e7260f63fa0177ff79626c7882838
Marios Petrov <118226559+marios-petrov@users.noreply.github.com>
2023-05-22 17:03:32 -0400

    Update databaseController.py
    Multi-threading madness

--- databaseController.py
+++ databaseController.py
@@ -1,9 +1,15 @@
 import sqlite3
+from threading import local
+
+# Thread-local storage for the database connection
+_thread_local = local()
 
 def create_connection(db_file):
-    conn = None;
+    conn = None
     try:
-        conn = sqlite3.connect(db_file, check_same_thread=False)
+        conn = sqlite3.connect(db_file)
+        conn.execute("PRAGMA foreign_keys = ON;")  # Enable foreign key constraints\
+        _thread_local.connection = conn
     except sqlite3.Error as e:
         print(e)
     return conn
@@ -22,6 +28,7 @@ def create_table(conn):
     except sqlite3.Error as e:
         print(f"Error creating table: {e}")
 
+
 def update_or_insert_call(conn, phone_number, timestamp, conversation_history, conversation_vector, communication_type):
     sql_update = """UPDATE calls SET conversation_history = ?, conversation_vector = ?, communication_type = ? WHERE phone_number = ? AND timestamp = ?;"""
     sql_insert = """INSERT INTO calls(phone_number, timestamp, conversation_history, conversation_vector, communication_type) VALUES(?, ?, ?, ?, ?);"""
@@ -39,19 +46,23 @@ def update_or_insert_call(conn, phone_number, timestamp, conversation_history, c
 
     return (phone_number, timestamp)
 
+
 def get_conversation_data(conn, phone_number):
     sql = """SELECT conversation_history, conversation_vector, communication_type FROM calls WHERE phone_number = ? ORDER BY timestamp DESC LIMIT 1;"""
     cur = conn.cursor()
     cur.execute(sql, (phone_number,))
     row = cur.fetchone()
     if row:
-        return row[0], row[1], row[2]
+        conversation_history = row[0]
+        conversation_vector = row[1]
+        communication_type = row[2]
+        return conversation_history, conversation_vector, communication_type
     else:
         return None, None, None
+
 
 def delete_all_calls(conn):
     sql = "DELETE FROM calls;"
     cur = conn.cursor()
     cur.execute(sql)
     conn.commit()
-
