01e4ef25abe74dd73b327e8d86ccd1170921e769
Marios Petrov <118226559+marios-petrov@users.noreply.github.com>
2023-05-22 17:05:57 -0400

    Create apiKeys.py
    Set your own keys

---
+++ apiKeys.py
@@ -0,0 +1,7 @@
+# Set your OpenAI API key
+openai_key = "SAMPLE"
+
+# Set your Twilio API credentials and phone number
+TWILIO_ACCOUNT_SID = "SAMPLE"
+TWILIO_AUTH_TOKEN = "SAMPLE"
+TWILIO_PHONE_NUMBER = "SAMPLE"
